5938b1f6560b6ac1b8f7e496f57bb52d952873af
Vivek Chaurasia <93462748+vivekchaurasia260@users.noreply.github.com>
2023-03-01 10:29:26 +0530

    Update data.py

--- data.py
+++ data.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 data = pd.read_csv("bank_branches.csv")
 df = pd.DataFrame(data)
-for i, ifs in enumerate(df.ifsc, start=0):
-    if ifs == "YESB0YUCB18":
-        print(df.loc[i])
+# for i, ifs in enumerate(df.ifsc, start=0):
+#     if ifs == "YESB0YUCB18":
+#         print(df.loc[i])
