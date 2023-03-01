22f914d06650aca46dfc909cf9b32abb7d00be31
Vivek Chaurasia <93462748+vivekchaurasia260@users.noreply.github.com>
2023-03-01 10:32:15 +0530

    Updated data.py
    Commented 3 lines which is to check the data from csv.

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 data = pd.read_csv("bank_branches.csv")
 df = pd.DataFrame(data)
+# Below lines is to check data coming from CSV.
 # for i, ifs in enumerate(df.ifsc, start=0):
 #     if ifs == "YESB0YUCB18":
 #         print(df.loc[i])
